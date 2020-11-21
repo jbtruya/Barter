bdf7c98576c7736eed3c0609e95ae36bda3372b2
JEB <juliusb.truya@gmail.com>
2020-11-21 15:25:07 +0800

    Changes: - added addlisting function

--- app/src/main/java/com/example/barter/addItem.java
+++ app/src/main/java/com/example/barter/addItem.java
@@ -10,14 +10,26 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.provider.MediaStore;
+import android.util.Base64;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
 import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.android.material.textfield.TextInputLayout;
+
+import java.io.ByteArrayOutputStream;
+import java.util.HashMap;
+import java.util.Map;
 
 public class addItem extends Fragment {
 
@@ -36,7 +48,12 @@ public class addItem extends Fragment {
     StringRequest stringRequest;
 
     Button bttn_selectImage;
+    Button bttn_addListing;
     ImageView selectedImage;
+
+    TextInputLayout edt_product_name;
+    TextInputLayout edt_listing_details;
+
 
     public addItem() {
         // Required empty public constructor
@@ -67,7 +84,11 @@ public class addItem extends Fragment {
         View view = inflater.inflate(R.layout.fragment_add_item, container, false);
 
         bttn_selectImage = view.findViewById(R.id.bttn_selectImage);
+        bttn_addListing = view.findViewById(R.id.bttn_addListing);
         selectedImage = view.findViewById(R.id.imageView_selectedImage);
+
+        edt_product_name = view.findViewById(R.id.edt_productName);
+        edt_listing_details = view.findViewById(R.id.edt_lisingDetails);
 
         bttn_selectImage.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -79,6 +100,14 @@ public class addItem extends Fragment {
             }
         });
 
+        bttn_addListing.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                addListing(edt_product_name.getEditText().getText().toString(),edt_listing_details.getEditText().getText().toString());
+
+            }
+        });
 
 
         return view;
@@ -100,5 +129,39 @@ public class addItem extends Fragment {
                 e.printStackTrace();
             }
         }
+    }
+    public void addListing(final String product_name, final String listing_details) {
+
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, byteArrayOutputStream);
+        byte[] imageBytes = byteArrayOutputStream.toByteArray();
+        final String imageString = Base64.encodeToString(imageBytes, Base64.DEFAULT);
+
+        stringRequest = new StringRequest(Request.Method.POST, "https://xototlprojects.com/AndroidPHP/addListingAndroid.php",
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Toast.makeText(getContext(),"Listing Created!",Toast.LENGTH_LONG).show();
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(getContext(),error.getMessage(),Toast.LENGTH_LONG).show();
+            }
+        })
+        {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                HashMap<String,String> map = new HashMap<>();
+
+                map.put("image",imageString);
+                map.put("product_name", product_name);
+                map.put("listing_details", listing_details);
+
+                return map;
+            }
+        };
+        requestQueue = Volley.newRequestQueue(getContext());
+        requestQueue.add(stringRequest);
     }
 }
